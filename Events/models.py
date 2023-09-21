29e5956737c151b0f185ca47185cd8fa83891206
Your Name <you@example.com>
2023-09-21 14:12:42 +0500

    Add media and static

--- Events/models.py
+++ Events/models.py
@@ -1,3 +1,7 @@
+from distutils.command import upload
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    event_image = models.ImageField(upload_to="Event_images/")
+    event_text = models.CharField(max_length=300)
